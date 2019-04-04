195d5cd9d276b42200208d1d26350111038374f8
Renne Sampaio <rennesampaio97@gmail.com>
2019-04-04 15:17:32 -0300

    Fix order of transformations on model matrix

--- src/main.cpp
+++ src/main.cpp
@@ -209,14 +209,16 @@ int main() {
             ImGui::End();
         }
 
-        model = glm::rotate(glm::mat4(1.0f), glm::radians(rotation.x),
+        model = glm::mat4(1.0f);
+
+        model = glm::translate(model, position);
+        model = glm::rotate(model, glm::radians(rotation.x),
                             glm::vec3(1.0, 0.0, 0.0));
         model = glm::rotate(model, glm::radians(rotation.y),
                             glm::vec3(0.0, 1.0, 0.0));
         model = glm::rotate(model, glm::radians(rotation.z),
                             glm::vec3(0.0, 0.0, 1.0));
         model = glm::scale(model, scale);
-        model = glm::translate(model, position);
 
         view = glm::translate(glm::mat4(1.0f), -camera_pos);
 
