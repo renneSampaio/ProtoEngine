1e46cf465aa9742c67120837d0992fbc84543ae2
Renne Sampaio <rennesampaio97@gmail.com>
2019-04-29 16:20:19 -0300

    Test multiple viewports

--- src/ProtoEngine/engineTest.cpp
+++ src/ProtoEngine/engineTest.cpp
@@ -2,6 +2,8 @@
 #include "Camera.hpp"
 #include "MeshComponent.hpp"
 #include "Node.hpp"
+#include "Scene.hpp"
+#include "Shader.hpp"
 
 #include "SDL2/SDL.h"
 
@@ -9,7 +11,6 @@ using namespace Proto;
 
 int main() {
     Engine::init("Teste", 0, 0, 400, 400);
-
     auto engine = Proto::Engine::getInstance();
 
     std::vector<Vertex> mesh{
@@ -23,28 +24,38 @@ int main() {
     };
 
     MeshComponent* mesh_component = new MeshComponent(mesh, indices);
-
     Camera cam(0.01f, 100.0f, engine->getMainWindow()->getAspectRatio(), 90.0f);
 
-    Node root;
-    root.addComponent(&cam);
-    root.setShader("shaders/test.vert", "shaders/test.frag");
+    Shader shader("shaders/test.vert", "shaders/test.frag");
 
     Node cameraNode;
     cameraNode.addComponent(&cam);
-    cameraNode.setShader("shaders/test.vert", "shaders/test.frag");
     cameraNode.setPosition(0.0, 0.0, 10.0);
 
     Node quadNode;
     quadNode.addComponent(mesh_component);
-    quadNode.setShader("shaders/test.vert", "shaders/test.frag");
+    quadNode.setShader(&shader);
     quadNode.setPosition(0.0f, 0.0f, 0.0f);
 
-    root.addChild(&quadNode);
-    root.addChild(&cameraNode);
+    Node quadNode2;
+    quadNode2.addComponent(mesh_component);
+    quadNode2.setShader(&shader);
+    quadNode2.setPosition(0.0f, 0.0f, -10.0f);
 
-    engine->setRoot(&root);
-    engine->setCamera(&cam);
+    Scene scene;
+    scene.addChild(&cameraNode);
+    scene.addChild(&quadNode);
+
+    Window* window = engine->getMainWindow();
+    window->getViewports()[0]->setScene(&scene);
+    window->getViewports()[0]->setCamera(&cam);
+    window->getViewports()[0]->setClearColor(0.0, 0.4, 0.4, 1.0);
+
+    Viewport view{10, 10, 100, 100};
+    window->addViewport(&view);
+    window->getViewports()[1]->setScene(&scene);
+    window->getViewports()[1]->setCamera(&cam);
+    window->getViewports()[1]->setClearColor(0.0, 1.0, 1.0, 1.0);
 
     engine->mainLoop();
 }
